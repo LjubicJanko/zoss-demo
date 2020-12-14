f41092b5f2a84ff4fcfeb386db3167545799e8a4
LjubicJanko <ljubicjanko1@gmail.com>
2020-12-14 21:00:56 +0100

    demo prepared store

--- angular-frontend/src/app/shared/modules/main.module.ts
+++ angular-frontend/src/app/shared/modules/main.module.ts
@@ -2,18 +2,23 @@ import { AccountModule } from './../../components/account/account.module';
 
 import { NgModule } from '@angular/core';
 import { UserModule } from 'src/app/components/user/user.module';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material';
+import { SnackbarComponent } from 'src/app/components/common/snackbar/snackbar.component';
 
 
 /*
 * Ovde se nalaze moduli koji grupisu osnovne grupe modula
 */
 @NgModule({
-  declarations: [],
+  declarations: [SnackbarComponent],
+  entryComponents: [SnackbarComponent],
   imports: [
     UserModule,
     AccountModule
     // ovde ce biti Home, Admin, User module
   ],
-  exports: []
+  exports: [],
+  providers: [    {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 5000, horizontalPosition: "right", verticalPosition: "bottom"},}
+]
 })
 export class MainModule { }
